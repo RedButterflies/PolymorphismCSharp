0ce6d3d41213276d3dff355e125d7fe3f8bd97e6
Aleksandra Olejarz <oluta525@gmail.com>
2023-08-18 03:15:11 +0200

    added classes deriving from a parent class with methods overriding the parent class methos

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//polymorphism 
+
+using System.Transactions;
+
+class Being
+{
+    public virtual void introduction()
+    {
+        Console.WriteLine("I am a being");
+    }
+
+}
+
+class Human : Being
+{
+    public string name;
+    public int age;
+    public string skinColour;
+    public double weight;
+    public string countryOfOrigin;
+
+    public Human(string name, int age, string skinColour, double weight, string countryOfOrigin)
+    {
+        this.name = name;
+        this.age = age;
+        this.skinColour = skinColour;
+        this.weight = weight;
+        this.countryOfOrigin = countryOfOrigin;
+    }
+
+    public override void introduction()
+    {
+        Console.WriteLine("I am a human");
+    }
+}
+
+class Alien : Being
+{ 
+
+    public string name;
+    public int age;
+    public string skinColour;
+    public double weight;
+    public string planet;
+
+    public Alien(string name, int age, string skinColour, double weight, string planet)
+    {
+        this.name = name;
+        this.age = age;
+        this.skinColour = skinColour;
+        this.weight = weight;
+        this.planet = planet;
+    }
+
+    public override void introduction()
+    {
+        Console.WriteLine("I am an alien from space");
+    }
+}
